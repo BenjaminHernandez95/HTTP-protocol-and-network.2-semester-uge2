f5031eeedeaeb049bed4cf147e8f7045af5f233e
Benjamin <bennobarrales@gmail.com>
2024-02-07 11:41:01 +0100

    Second

--- HTTP-Protocol-And-Network/src/main/java/org/example/SimpleServer.java
+++ HTTP-Protocol-And-Network/src/main/java/org/example/SimpleServer.java
@@ -1,4 +1,7 @@
+package org.example;
+
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -6,6 +9,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class SimpleServer {
+
+
+    public void start(int port){
+        try{
+        ServerSocket serverSocket = new ServerSocket(port);
+        } catch (IOException e){
+            System.out.println("Something");
+        }
+    }
 
     private String getFile(String ressource) {
         ressource = reformatRessource(ressource); // remove leading / and add .html if not present
@@ -15,7 +27,7 @@ public class SimpleServer {
 
         try {
             // Get the URI of the resource using getResource
-            URI resourceUri = RequestFileServer.class.getClassLoader().getResource(ressource).toURI();
+            URI resourceUri = SimpleServer.class.getClassLoader().getResource(ressource).toURI();
             // Use Paths.get with the URI to create a Path
             Path resourcePath = Paths.get(resourceUri);
 
@@ -33,7 +45,15 @@ public class SimpleServer {
     }
 
     private String reformatRessource(String ressource) {
-        return;
+        return null;
+    }
+
+    public void stop(){
+        try{
+
+        }catch (){
+
+        }
     }
 }
 
